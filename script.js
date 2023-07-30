1f9b28ec852f0b30af5238b660a31adcef45bd79
Sakshi Giri <sakshi.giri2910@gmail.com>
2023-07-30 14:04:50 +0530

    Clone repo of portfolio

---
+++ script.js
@@ -0,0 +1,51 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+ 
+function opentab(tabname)
+  {
+    for(tablink of tablinks)
+    {
+      tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents)
+    {
+        tabcontent.classList.remove("tab-skill");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("tab-skill");
+  }
+
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+
+
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+
+                                    
